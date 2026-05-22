3288759c5d70285360a24060bb12ea53a82cf468
seif1125 <seifammar1125@gmail.com>
2026-05-22 14:46:33 +0300

    Fix locale default

--- middleware.js
+++ middleware.js
@@ -1,9 +1,17 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
-export default createMiddleware(routing);
+export default createMiddleware({
+  // A list of all locales that are supported
+  locales: ['en', 'ar'],
+  
+  // Used when no locale matches
+  defaultLocale: 'en', // <--- Change this from 'ar' to 'en'
+  
+  // Optional: If you don't want '/en' to show in the URL bar for the default language
+  localePrefix: 'as-needed' 
+});
 
 export const config = {
-  // Match only internationalized pathnames
   matcher: ['/', '/(ar|en)/:path*']
 };
